283cb711b4333bbb267ac22f6fe6161a525e6a11
Hannahvic <hannahvl1987@hotmail.com>
2020-09-21 16:34:19 +0100

    Updates

---
+++ precache-manifest.32d3864b6addb9810daf4392c04b358c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4d58b32e7840f0dbe202e80f9fa580",
+    "url": "/countries/index.html"
+  },
+  {
+    "revision": "aecbabb01ff330fce50c",
+    "url": "/countries/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "73986697be75440412b8",
+    "url": "/countries/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "aecbabb01ff330fce50c",
+    "url": "/countries/static/js/2.fd2ae620.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/countries/static/js/2.fd2ae620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73986697be75440412b8",
+    "url": "/countries/static/js/main.7787ef66.chunk.js"
+  },
+  {
+    "revision": "98a2b82ebb3a74cbde09",
+    "url": "/countries/static/js/runtime-main.e148dddc.js"
+  }
+]);
